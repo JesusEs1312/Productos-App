2f568ad7cd5d3488d3d38fe27b5743c29a853672
JesusEs1312 <estrada.jesus1312@gmail.com>
2022-01-10 11:42:53 -0600

    Se realizo el componente de Sign Up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+// import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+// import theme from './theme/theme';
+// import { Button, TextField } from '@material-ui/core';
+import SignUp from './componentes/Seguridad/SignUp';
+//Componente React (Retorno un componente grafico)
+//Utilizamos props para obtener los atributos del componente padre
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <MuiThemeProvider theme={theme}>
+        <h1>Proyecto en blanco</h1>
+        <TextField variant="outlined"></TextField>
+        <Button variant="contained" color="primary">Puchale</Button>
+      </MuiThemeProvider>
+      */}
+      <SignUp></SignUp>
     </div>
   );
 }
---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+import  {createTheme} from '@material-ui/core/styles';
+
+//Constante que tendra los colores de la interfaz
+const theme = createTheme ({
+    palette : {
+        primary: {
+            light : '#4f83cc',
+            main  : '#0277bd',
+            dark  : '#002f6c'
+        }
+    }
+});
+
+export default theme;
